114cf662984bb4c45728accbc5d4d92a1454e7d1
Cukehater <cukehater@gmail.com>
2026-02-20 18:33:51 +0900

    [Three.js Journey] 스터디

--- src/exercise/05-animations/script.js
+++ src/exercise/05-animations/script.js
@@ -1,46 +1,54 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // Sizes
 const sizes = {
   width: 800,
   height: 600,
-}
+};
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
 
-const clock = new THREE.Clock()
+let previousTime = Date.now();
+const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime();
+  const currentTime = Date.now();
+  const deltaTime = currentTime - previousTime;
+  previousTime = currentTime;
 
-  //   mesh.rotation.y = elapsedTime * Math.PI * 2
-  mesh.position.y = Math.cos(elapsedTime)
-  mesh.position.x = Math.sin(elapsedTime)
-  renderer.render(scene, camera)
+  // mesh.rotation.y = elapsedTime * Math.PI * 2; // 1초에 1회전
+  // mesh.position.y = Math.cos(elapsedTime);
+  // mesh.position.x = Math.sin(elapsedTime);
 
-  window.requestAnimationFrame(tick)
-}
+  camera.position.y = Math.cos(elapsedTime);
+  camera.position.x = Math.sin(elapsedTime);
 
-tick()
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
--- src/exercise/06-cameras/script.js
+++ src/exercise/06-cameras/script.js
@@ -2,11 +2,6 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 /**
- * Base
- */
-// Canvas
-
-/**
  * Cursor
  */
 const cursor = {
@@ -33,24 +28,21 @@ const scene = new THREE.Scene();
 // Object
 const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    // wireframe: true
-  }),
+  new THREE.MeshBasicMaterial()
 );
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-/* const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio,
-  1 * aspectRatio,
-  1,
-  -1,
-  0.1,
-  100,
-); */
+const aspectRatio = sizes.width / sizes.height;
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
 // camera.position.x = 2;
 // camera.position.y = 2;
 camera.position.z = 4;
@@ -72,13 +64,12 @@ renderer.setSize(sizes.width, sizes.height);
 // const clock = new THREE.Clock();
 
 const tick = () => {
-  //   const elapsedTime = clock.getElapsedTime();
+  // const elapsedTime = clock.getElapsedTime();
 
-  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  //   camera.position.y = cursor.y * 5;
-
-  //   camera.lookAt(mesh.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.lookAt(mesh.position);
 
   // Update objects
   //   mesh.rotation.y = elapsedTime;
@@ -86,10 +77,7 @@ const tick = () => {
   // Update controls
   controls.update();
 
-  // Render
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
